eaa67a7e30720033329cbfba34cbb0ae3655b670
djtesla <djtesla@gmailcom>
2021-03-16 17:04:54 +0100

    DigitsCounter kész commit

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,6 +1,26 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class DigitsCounter {
+
+    public static final List<Character> DIGITS = Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+
+    public int getCountOfDigits(String s) {
+        Set<Character> differentDigits = new HashSet<>();
+        if (s == null) {
+            return 0;
+        }
+        for (char c : s.toCharArray()) {
+            if (DIGITS.contains(c)) {
+                differentDigits.add(c);
+            }
+        }
+        return differentDigits.size();
+    }
 
 
 }
